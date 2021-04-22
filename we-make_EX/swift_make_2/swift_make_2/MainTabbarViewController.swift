e34da25030bee36c911c1e3da5da0f48460e6152
project-chodanta <project.chodanta@gmail.com>
2021-04-22 14:47:26 +0900

    swift_make_2 추가

---
+++ we-make_EX/swift_make_2/swift_make_2/MainTabbarViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainTabbarViewController.swift
+//  swift_make_2
+//
+//
+//
+
+import UIKit
+
+class MainTabbarViewController : UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .orange
+    }
+}
